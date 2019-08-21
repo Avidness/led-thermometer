48c2b25c8fa3783b1f63fe3d5c02756e1c3e5d15
avidness <adavidness@gmail.com>
2019-08-21 00:35:22 +0000

    use color and image helpers

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from sense_hat import SenseHat
-from helpers import led_images as images
+from helpers import weather_images as images, colors
 import urllib.request, json, threading, yaml
+
+class PixelColor: 
+  def __init__(self, r, g, b):
+    self.r = r
+    self.g = g
+    self.b = b
 
 def run(interval, url):
   threading.Timer(interval, run, [interval, url]).start()
-  req_temp(url)
+  tempF = get_temp(url)
+  set_display(tempF)
 
-def req_temp(url):
+def get_temp(url):
   resp = urllib.request.urlopen(url)
   data = resp.read()
   encoding = resp.info().get_content_charset('utf-8')
@@ -14,22 +21,25 @@ def req_temp(url):
   
   tempC = jdata['main']['temp']
   tempF = (tempC * 9/5) + 32
-
-  set_display(str(int(round(tempF + .5))))
+  return str(int(round(tempF + .5)))
 
 def set_display(tempF):
   print(tempF)
   sense = SenseHat()
   sense.set_rotation(180)
-  sense.show_message(tempF)
 
-  #top = 7
-  #bot = 0
+  color = get_color(tempF)
+
+  top = 7
+  bot = 0
+  test = images.test_image(colors.g)
+  sense.set_pixels(test)
+  sense.set_pixel(top, bot, color.r, color.b, color.g)
+
+def get_color(tempF):
   #red = 255,0,0
   #blue = 0,0,255
-
-  sense.set_pixels(images.cat_eyes_open)
-  sense.set_pixel(0,7, 255,255,255)
+  return PixelColor(0,0,255)
 
 with open('config.yml', 'r') as file:
   config = yaml.load(file, Loader=yaml.FullLoader)
