04683a30a68d6951890a96605a4d7866426d33be
avidness <adavidness@gmail.com>
2019-08-21 00:32:59 +0000

    add color and image helpers

---
+++ helpers/weather_images.py
@@ -0,0 +1,21 @@
+r = [255,0,0]
+o = [255,165,0]
+y = [255,255,0]
+g = [0,255,0]
+b = [0,0,255]
+i = [75,0,130]
+v = [159,0,255]
+e = [0,0,0]
+w = [255,255,255]
+ 
+def test_image(r):
+	return [
+    e,e,e,e,e,e,e,e,
+    e,e,r,r,r,r,e,e,
+    e,r,r,r,r,r,r,e,
+    e,r,r,e,e,r,r,e,
+    e,r,r,e,e,r,r,e,
+    e,r,r,r,r,r,r,e,
+    e,e,r,r,r,r,e,e,
+    e,e,e,e,e,e,e,e
+  ];
